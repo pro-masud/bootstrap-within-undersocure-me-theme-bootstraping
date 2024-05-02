b11051932112f95049aef77f6cbbbf8adfaf1631
pro-masud <promasudbd@gmail.com>
2024-05-02 21:36:03 +0600

    understrap bootstrap theme convert to mrwoocommerce theme

---
+++ sidebar-templates/sidebar-right.php
@@ -0,0 +1,26 @@
+<?php
+
+if(is_shop()){
+	$right_sidebar = "shop-sidebar";
+}else{
+	$right_sidebar = "right-sidebar";
+}
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! is_active_sidebar( $right_sidebar ) ) {
+	return;
+}
+
+// when both sidebars turned on reduce col size to 3 from 4.
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+?>
+
+<?php if ( 'both' === $sidebar_pos ) : ?>
+	<div class="col-md-3 widget-area" id="right-sidebar">
+<?php else : ?>
+	<div class="col-md-4 widget-area" id="right-sidebar">
+<?php endif; ?>
+<?php dynamic_sidebar( $right_sidebar ); ?>
+
+</div><!-- #right-sidebar -->
